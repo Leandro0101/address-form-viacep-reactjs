cfa5fc85ce4bebcad486f3b5e67c6816ce6ccc46
Leandro0101 <leandrovieira3306@gmail.com>
2020-08-31 02:07:06 -0300

    adding formik

--- src/pages/address/index.js
+++ src/pages/address/index.js
@@ -2,37 +2,58 @@ import React from 'react';
 import './styles.css';
 import { Formik, Field, Form } from 'formik';
 const Address = () => {
+
+    const onSubmit = (values) => {
+        console.log(values);
+    }
+
+
     return (
         <div>
-            <form className="form-adress">
-                <div className="form-group">
-                    <label>CEP</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Logradouro</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Número</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Bairro</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Cidade</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Estado</label>
-                    <input type="text" className="form-control" />
-                </div>
-                <div className="form-group">
-                    <button className="btn">Enviar</button>
-                </div>
-            </form>
+            <Formik
+                onSubmit={onSubmit}
+                initialValues={{
+                    cep: "",
+                    log: "",
+                    number: "",
+                    district: "",
+                    city: "",
+                    state: "",
+                }}
+
+                render={({ values }) => (
+                    <Form className="form-adress">
+                        <div className="form-group">
+                            <label>CEP</label>
+                            <Field name="cep" type="text" className="form-control" value={values.cep} />
+                        </div>
+                        <div className="form-group">
+                            <label>Logradouro</label>
+                            <Field name="log" type="text" className="form-control" value={values.log} />
+                        </div>
+                        <div className="form-group">
+                            <label>Número</label>
+                            <Field name="number" type="text" className="form-control" value={values.number} />
+                        </div>
+                        <div className="form-group">
+                            <label>Bairro</label>
+                            <Field name="district" type="text" className="form-control" value={values.district} />
+                        </div>
+                        <div className="form-group">
+                            <label>Cidade</label>
+                            <Field name="city" type="text" className="form-control" value={values.city} />
+                        </div>
+                        <div className="form-group">
+                            <label>Estado</label>
+                            <Field name="state" type="text" className="form-control" value={values.state} />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn">Enviar</button>
+                        </div>
+                    </Form>
+                )}
+            />
+
         </div>
     )
 }
