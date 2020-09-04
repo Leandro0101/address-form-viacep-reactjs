7757e1885904f6649d0bf9032555673ee6ba45c2
Leandro0101 <leandrovieira3306@gmail.com>
2020-09-04 00:36:13 -0300

    add axios

--- src/pages/address/index.js
+++ src/pages/address/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './styles.css';
 import { Formik, Field, Form } from 'formik';
+import api from '../../services/api';
 const Address = () => {
 
     const onSubmit = (values) => {
         console.log(values);
     }
 
-    const onBlurCep = (ev, setFieldValue, resetForm) => {
+    const onBlurCep = async (ev, setFieldValue, resetForm) => {
         const { value } = ev.target;
 
         const cep = value?.replace(/[^0-9]/g, '');
@@ -17,30 +18,24 @@ const Address = () => {
             return;
         }
 
-        fetch(`https://viacep.com.br/ws/${cep}/json/`).
-            then((response) => response.json()).
-            then((data) => {
-                setFieldValue('log', data.logradouro);
-                setFieldValue('number', data.numero);
-                setFieldValue('district', data.bairro);
-                setFieldValue('city', data.localidade);
-                setFieldValue('state', data.uf);
-            }
-            );
+        const response = await api.get(`${cep}/json/`);
+        const { data } = response;
+
+        setFieldValue("bairro", data.bairro);
+        setFieldValue("localidade", data.localidade);
+        setFieldValue("uf", data.uf);
+        setFieldValue("logradouro", data.logradouro);        
     }
-
-
     return (
         <div>
             <Formik
                 onSubmit={onSubmit}
                 initialValues={{
                     cep: "",
-                    log: "",
-                    number: "",
-                    district: "",
-                    city: "",
-                    state: "",
+                    logradouro: "",
+                    bairro: "",
+                    localidade: "",
+                    uf: "",
                 }}
 
                 render={({ values, setFieldValue, resetForm }) => (
@@ -51,19 +46,19 @@ const Address = () => {
                         </div>
                         <div className="form-group">
                             <label>Logradouro</label>
-                            <Field name="log" type="text" className="form-control" value={values.log} />
+                            <Field name="logradouro" type="text" className="form-control" value={values.logradouro} />
                         </div>
                         <div className="form-group">
                             <label>Bairro</label>
-                            <Field name="district" type="text" className="form-control" value={values.district} />
+                            <Field name="bairro" type="text" className="form-control" value={values.bairro} />
                         </div>
                         <div className="form-group">
                             <label>Cidade</label>
-                            <Field name="city" type="text" className="form-control" value={values.city} />
+                            <Field name="localidade" type="text" className="form-control" value={values.localidade} />
                         </div>
                         <div className="form-group">
                             <label>Estado</label>
-                            <Field name="state" type="text" className="form-control" value={values.state} />
+                            <Field name="uf" type="text" className="form-control" value={values.uf} />
                         </div>
                         <div className="form-group">
                             <button type="submit" className="btn">Enviar</button>
